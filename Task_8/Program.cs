4ca551087a2b636601b8bd12a373ab9a09a96cbf
Bogdan <bogdan@mail.com>
2023-03-26 12:27:39 +0300

    Четные числа от 1 до N

---
+++ Task_8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число:");
+// считываем число N
+int n = int.Parse(Console.ReadLine());
+
+// начинаем с 2, т.к. 1 не является четным числом
+for (int i = 2; i <= n; i += 2)
+{
+  Console.Write(i + " ");
+}
