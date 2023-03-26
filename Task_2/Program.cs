a22c98082411ebe5f04c8fa65e1cb1d26207e226
Bogdan <bogdan@mail.com>
2023-03-26 11:32:03 +0300

    Большее из двух чисел

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+  Console.WriteLine($"Число {num1} больше числа {num2}");
+}
+else if (num1 < num2)
+{
+  Console.WriteLine($"Число {num2} больше числа {num1}");
+}
+else
+{
+  Console.WriteLine($"Число {num1} равно числу {num2}");
+}
