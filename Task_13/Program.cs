30a91f1aa481b91cc119e3a15f6048caad41ac88
Bogdan <bogdan@mail.com>
2023-04-01 10:11:41 +0300

    Третья цифра числа

---
+++ Task_13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+string input = Console.ReadLine();
+if (input.Length >= 3)
+{
+  Console.WriteLine($"Третья цифра числа: {input[2]}");
+}
+else
+{
+  Console.WriteLine("В числе меньше трех цифр.");
+}
