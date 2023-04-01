00899726cd7356a30c0fd2c2ada5b25c98a32e53
Bogdan <bogdan@mail.com>
2023-04-01 10:02:22 +0300

    Вторая цифра числа

---
+++ Task_10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int secondDigit = (number / 10) % 10;
+
+Console.WriteLine("Вторая цифра числа: " + secondDigit);
