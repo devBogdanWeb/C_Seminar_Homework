f610963fceef070131c6bb5aac634d808d713455
Bogdan <bogdan@mail.com>
2023-04-01 10:37:56 +0300

    Рабочий или выходной день недели

---
+++ Task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите номер дня недели: ");
+int dayOfWeek = int.Parse(Console.ReadLine());
+
+if (dayOfWeek >= 1 && dayOfWeek <= 5)
+{
+  Console.WriteLine("Это рабочий день.");
+}
+else if (dayOfWeek == 6 || dayOfWeek == 7)
+{
+  Console.WriteLine("Это выходной день.");
+}
+else
+{
+  Console.WriteLine("Введите число от 1 до 7");
+}
