cfa77da943ce274f7373fe0597e4e283878f5761
Bogdan <bogdan@mail.com>
2023-04-02 12:49:23 +0300

    Расстояние между двумя точками в 3D пространстве

---
+++ Task_21/Program.cs
@@ -0,0 +1,19 @@
+﻿// ввод координат первой точки
+Console.Write("Введите координаты первой точки через запятую: ");
+string[] coords1 = Console.ReadLine().Split(',');
+double x1 = double.Parse(coords1[0]);
+double y1 = double.Parse(coords1[1]);
+double z1 = double.Parse(coords1[2]);
+
+// ввод координат второй точки
+Console.Write("Введите координаты второй точки через запятую: ");
+string[] coords2 = Console.ReadLine().Split(',');
+double x2 = double.Parse(coords2[0]);
+double y2 = double.Parse(coords2[1]);
+double z2 = double.Parse(coords2[2]);
+
+// вычисление расстояния между точками
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+// вывод результата
+Console.WriteLine("Расстояние между точками: " + distance.ToString("F2"));
