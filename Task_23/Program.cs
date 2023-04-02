458b93445747a0654387e476c2b80799f8f69fe3
Bogdan <bogdan@mail.com>
2023-04-02 12:55:19 +0300

    Таблица кубов чисел от 1 до N

---
+++ Task_23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Таблица кубов чисел от 1 до " + n + ":");
+for (int i = 1; i <= n; i++)
+{
+  int cube = i * i * i;
+  Console.WriteLine(i + "^3 = " + cube);
+}
