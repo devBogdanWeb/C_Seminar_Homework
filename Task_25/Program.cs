a6d587a5ae3877ff1a0f6a72b1bd418527cbb496
Bogdan <bogdan@mail.com>
2023-04-08 13:02:17 +0300

    Число A в степени B

---
+++ Task_25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число A:");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите натуральную степень B:");
+int b = int.Parse(Console.ReadLine());
+
+int result = 1;
+for (int i = 0; i < b; i++)
+{
+  result *= a;
+}
+
+Console.WriteLine("{0} в степени {1} равно {2}", a, b, result);
