76b02c03bd126e1718052f7422a7a299c2275ede
Bogdan <bogdan@mail.com>
2023-04-08 13:35:41 +0300

    Массив из 8 значений от пользователя

---
+++ Task_29/Program.cs
@@ -0,0 +1,14 @@
+﻿char[] arr = new char[8]; // создание массива из 8 символов
+
+for (int i = 0; i < arr.Length; i++)
+{
+  Console.Write($"Введите символ #{i + 1}: ");
+  arr[i] = Console.ReadKey().KeyChar; // чтение символа с консоли
+  Console.WriteLine(); // переход на новую строку
+}
+
+Console.WriteLine("Вы ввели следующие символы:");
+foreach (char num in arr)
+{
+  Console.Write($"{num} "); // вывод символов на консоль
+}
