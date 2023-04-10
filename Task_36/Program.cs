d592cef5f2172b73114c2f984e70a91761984e6c
Bogdan <bogdan@mail.com>
2023-04-10 17:40:15 +0300

    Сумма элементов массива на нечетных позициях

---
+++ Task_36/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array = new int[10]; // создаем массив из 10 элементов
+
+Random random = new Random(); // создаем объект генератора случайных чисел
+
+// заполняем массив случайными числами
+for (int i = 0; i < array.Length; i++)
+{
+  array[i] = random.Next(100); // генерируем случайное число от 0 до 99
+}
+
+// выводим массив на экран
+Console.WriteLine("Массив:");
+foreach (int element in array)
+{
+  Console.Write(element + " ");
+}
+Console.WriteLine();
+
+int sum = 0;
+// находим сумму элементов, стоящих на нечетных позициях
+for (int i = 1; i < array.Length; i += 2)
+{
+  sum += array[i];
+}
+
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {sum}");
