9035b7529ab7c44073a99c0a2e53b22eb80c37bd
Bogdan <bogdan@mail.com>
2023-04-15 11:01:03 +0300

    Количество чисел больше 0

---
+++ Task_41/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите количество чисел M: ");
+int m = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+for (int i = 0; i < m; i++)
+{
+  Console.Write($"Введите число {i + 1}: ");
+  int num = int.Parse(Console.ReadLine());
+  if (num > 0)
+  {
+    count++;
+  }
+}
+
+Console.WriteLine($"Количество чисел больше 0: {count}");
