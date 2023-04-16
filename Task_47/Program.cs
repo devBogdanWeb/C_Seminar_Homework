e73b2ff27a5d2a657303374e09800528ade34474
Bogdan <bogdan@mail.com>
2023-04-16 13:20:52 +0300

    Двумерный массив размером m×n

---
+++ Task_47/Program.cs
@@ -0,0 +1,25 @@
+﻿int m = 3; // количество строк
+int n = 4; // количество столбцов
+
+// Создание двумерного массива
+double[,] array = new double[m, n];
+
+// Заполнение массива случайными числами
+Random random = new Random();
+for (int i = 0; i < m; i++)
+{
+  for (int j = 0; j < n; j++)
+  {
+    array[i, j] = random.NextDouble();
+  }
+}
+
+// Вывод массива на консоль
+for (int i = 0; i < m; i++)
+{
+  for (int j = 0; j < n; j++)
+  {
+    Console.Write("{0:f2}\t", array[i, j]);
+  }
+  Console.WriteLine();
+}
