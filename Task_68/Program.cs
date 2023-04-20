01258f82181128079627db56049fe4df1485308f
Bogdan <bogdan@mail.com>
2023-04-20 21:51:47 +0300

    Вычисление функции Аккермана с помощью рекурсии

---
+++ Task_68/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+  static int Ackermann(int m, int n)
+  {
+    if (m == 0)
+    {
+      return n + 1;
+    }
+    else if (n == 0)
+    {
+      return Ackermann(m - 1, 1);
+    }
+    else
+    {
+      return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+  }
+
+  static void Main(string[] args)
+  {
+    int m = 2;
+    int n = 3;
+    int result = Ackermann(m, n);
+    Console.WriteLine($"A({m}, {n}) = {result}");
+
+    m = 3;
+    n = 2;
+    result = Ackermann(m, n);
+    Console.WriteLine($"A({m}, {n}) = {result}");
+  }
+}
