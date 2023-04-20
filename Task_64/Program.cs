427f7f89e2a4a85fd5fef1cf12825e081944ad9b
Bogdan <bogdan@mail.com>
2023-04-20 21:41:56 +0300

    Натуральные числа в промежутке от N до 1

---
+++ Task_64/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int n = 8;
+    PrintNumbers(n);
+  }
+
+  public static void PrintNumbers(int n) {
+    if (n < 1) {
+      return;
+    }
+    Console.Write(n + " ");
+    PrintNumbers(n - 1);
+  }
+}
