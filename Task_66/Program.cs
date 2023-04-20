801b018df58f6de151d633f0c56bde24d5f5e4cd
Bogdan <bogdan@mail.com>
2023-04-20 21:46:14 +0300

    Сумма натуральных элементов в промежутке от M до N

---
+++ Task_66/Program.cs
@@ -0,0 +1,10 @@
+﻿int M = 1;
+int N = 15;
+int sum = 0;
+
+for (int i = M; i <= N; i++)
+{
+  sum += i;
+}
+
+Console.WriteLine("Сумма натуральных элементов от " + M + " до " + N + " равна " + sum);
